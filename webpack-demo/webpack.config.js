eef816e90d66da165f1b6d235de4f97e681629b6
Pat <alfonsopatrisha@gmail.com>
2022-07-30 16:32:32 -0700

    Add 'Maybe Coffee' font files

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -7,5 +7,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   module: {rules: [{test: /\.css$/i, use: ['style-loader', 'css-loader']},
-    {test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource'}]}
+    {test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource'},
+    {test: /\.(woff|woff2|eot|ttf|otf)$/i, type: 'asset/resource'}]}
 };
