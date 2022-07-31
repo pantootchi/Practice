39a8aba7047a62c2cd67ccc711881d75936feaf4
Pat <alfonsopatrisha@gmail.com>
2022-07-30 18:21:13 -0700

    Add data loaders, data files, and import to index.js

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -2,6 +2,8 @@ import _ from 'lodash';
 import myName from './myName';
 import './style.css';
 import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
     const element = document.createElement('div');
@@ -16,8 +18,11 @@ function component() {
     //Add the image to our existing div.
     const myIcon = new Image();
     myIcon.src = Icon;
-
     element.appendChild(myIcon);
+
+    console.log(Data);
+    console.log(Notes);
+    
     return element;
   }
   
