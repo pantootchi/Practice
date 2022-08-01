6ad6ad81dfdabcd31593eb910187b6e218335de1
Pat <alfonsopatrisha@gmail.com>
2022-08-01 16:37:39 -0700

    Install html webpack plugin

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -8,6 +8,8 @@ import toml from './data.toml';
 import yaml from './data.yaml';
 import json from './data.json5';
 
+import printMe from './print.js';
+
 console.log(toml.title);
 console.log(toml.owner.name);
 
@@ -19,21 +21,27 @@ console.log(json.owner.name);
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
   
     // Lodash, currently included via a script, is required for this line to work
     // Lodash, now imported by this script
-    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    element.textContent = myName('Cody');
-    element.classList.add('hello');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
+    // element.textContent = myName('Cody');
+    // element.classList.add('hello');
 
     //Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
+    // const myIcon = new Image();
+    // myIcon.src = Icon;
+    // element.appendChild(myIcon);
 
-    console.log(Data);
-    console.log(Notes);
+    // console.log(Data);
+    // console.log(Notes);
     
     return element;
   }
