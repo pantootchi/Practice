41b22dfbba51902095317214e77d52f0682fea62
Pat <alfonsopatrisha@gmail.com>
2022-08-01 19:44:56 -0700

    Install webpack-dev-server

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -5,14 +5,17 @@ const json5 = require('json5');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js'
   },
-  plugins: [new HtmlWebpackPlugin({title: 'Output Management'})],
+  devtool: 'inline-source-map',
+  plugins: [new HtmlWebpackPlugin({title: 'Development'})],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
   module: {rules: [{test: /\.css$/i, use: ['style-loader', 'css-loader']},
     {test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource'},
