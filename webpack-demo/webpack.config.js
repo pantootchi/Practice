4d3a5a8b2bbb1072d76d2867a71daa5de8538d29
Pat <alfonsopatrisha@gmail.com>
2022-08-01 22:36:35 -0700

    Update package.json and webpack configuration

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -11,11 +11,17 @@ module.exports = {
     print: './src/print.js'
   },
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist'
+  },
   plugins: [new HtmlWebpackPlugin({title: 'Development'})],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true
+  },
+  optimization: {
+    runtimeChunk: 'single'
   },
   module: {rules: [{test: /\.css$/i, use: ['style-loader', 'css-loader']},
     {test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource'},
