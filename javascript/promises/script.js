740d536d7ae992fdd94712561a25d235ce5717c6
Pat <alfonsopatrisha@gmail.com>
2022-11-02 16:46:20 -0700

    Edit script.js

--- javascript/promises/script.js
+++ javascript/promises/script.js
@@ -1,15 +1,15 @@
 import loadImage from './load-image';
 
-    let addImg = (src) => {
-        let imgElement = document.createElement("img");
-        imgElement.srs = src;
-        document.body.appendChild(imgElement)
-    }
+let addImg = (src) => {
+    let imgElement = document.createElement("img");
+    imgElement.srs = src;
+    document.body.appendChild(imgElement)
+}
 
-    Promise.all([
-        loadImage('./cat1.jpg'),
-        loadImage('./cat2.jpg'),
-        loadImage('./cat3.jpg')
-    ]).then((images) => {
-        images.forEach(img => addImg(img.src))
+Promise.all([
+    loadImage('./cat1.jpg'),
+    loadImage('./cat2.jpg'),
+    loadImage('./cat3.jpg')
+]).then((images) => {
+    images.forEach(img => addImg(img.src))
 })
